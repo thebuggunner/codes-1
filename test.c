b6e112ccac810dda6b0e0f4ab75c5568a890dda6
thebuggunner <42600386+thebuggunner@users.noreply.github.com>
2018-10-24 14:05:05 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+ bool b=5;
+  if(b)
+   printf("a");
+  if(b=true)
+   printf("b");
+  if(!b)
+   printf("c");
+  if(b=false);
+   printf("d");
+  else
+   printf("done");
+   
+  return 0;
+} 
